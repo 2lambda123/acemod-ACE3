0a1744d52fc9353fdad14baaf6bf4fda9f11a9dd
Restyled.io <commits@restyled.io>
2024-01-31 11:34:43 +0000

    Restyled by astyle

--- addons/scuba/script_component.hpp
+++ addons/scuba/script_component.hpp
@@ -8,11 +8,11 @@
 // #define ENABLE_PERFORMANCE_COUNTERS
 
 #ifdef DEBUG_ENABLED_SCUBA
-    #define DEBUG_MODE_FULL
+#define DEBUG_MODE_FULL
 #endif
 
 #ifdef DEBUG_SETTINGS_SCUBA
-    #define DEBUG_SETTINGS DEBUG_SETTINGS_SCUBA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_SCUBA
 #endif
 
 #include "\z\ace\addons\main\script_macros.hpp"
